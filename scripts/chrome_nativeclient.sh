cf00ac9da5bf75f45902610d6bd9e212bbdd2f64
Kristov Atlas <kristovatlas@gmail.com>
2016-07-05 18:14:41 -0400

    add config: disable native client in chrome
    QA: fixes broken config as expected

--- scripts/chrome_nativeclient.sh
+++ scripts/chrome_nativeclient.sh
@@ -2,9 +2,9 @@
 # Description: Patches Chrome "Preferences" file if needed. The setting for
 # plugins.plugins_list appears to be set to "False" or "[]" sometimes rather
 # than an arrray of objects, but we always want the latter. Once standardized,
-# Shockwave Flash will be disabled.
+# Chrome's Native Client will be disabled.
 #
-# Usage: chrome_flash.sh location-of-preferences-file
+# Usage: chrome_nativeclient.sh location-of-preferences-file
 
 # set working directory to the one containing this script so it can find other
 # scripts.
@@ -15,32 +15,34 @@ FILE=$1
 echo "DEBUG: FILE = $FILE"
 
 if [ $# -ne 1 ] ; then
-    echo "Usage: chrome_flash.sh location-of-preferences-file"
+    echo "Usage: chrome_nativeclient.sh location-of-preferences-file"
     exit
 fi
 
 #http://www.linuxjournal.com/content/return-values-bash-functions
 function plugins_list_value {
-    echo DEBUG: python chrome_defaults.py read "$FILE" plugins.plugins_list
     VALUE=$(python chrome_defaults.py read "$FILE" plugins.plugins_list)
     echo "$VALUE"
 }
 
 function is_list_missing {
-    echo DEBUG: python chrome_defaults.py read "$FILE" plugins.plugins_list
     MISSING=$(python chrome_defaults.py read "$FILE" plugins.plugins_list | grep -c "The attribute 'plugins\.plugins_list' does not exist")
     echo "$MISSING"
+}
+
+function native_client_missing {
+    NCPRESENT=$(python chrome_defaults.py read "$FILE" plugins.plugins_list | grep -c '"name": "Native Client"')
+    echo "$NCPRESENT"
 }
 
 #initialize
 plugins_list_value
 is_list_missing
-if [ "$VALUE" = "False" ] || [ "$VALUE" = "[]" ] || [[ $MISSING = "1" ]] ; then
+native_client_missing
+if [ "$VALUE" = "False" ] || [ "$VALUE" = "[]" ] || [[ $MISSING = "1" ]] || [[ $NCPRESENT = "0" ]] ; then
     python chrome_defaults.py delete "$FILE" plugins.plugins_list
     python chrome_defaults.py write "$FILE" plugins.plugins_list -json '[{"enabled": false, "version": "", "name": "Shockwave Flash"},{"enabled": false, "version": "", "name": "Adobe Flash Player"},{"enabled": false, "version": "", "name": "Native Client"}]'
 fi
 
 #write correct value
-echo DEBUG: python chrome_defaults.py write-array "$FILE" plugins.plugins_list enabled -bool false where name -string "Shockwave Flash"
-python chrome_defaults.py write-array "$FILE" plugins.plugins_list enabled -bool false where name -string "Shockwave Flash"
-python chrome_defaults.py write-array "$FILE" plugins.plugins_list enabled -bool false where name -string "Adobe Flash Player"
+python chrome_defaults.py write-array "$FILE" plugins.plugins_list enabled -bool false where name -string "Native Client"
